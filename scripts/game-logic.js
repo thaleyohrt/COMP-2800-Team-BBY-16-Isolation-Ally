e0ecc80f310e182d39b2a8d371cbc6470756f293
samsonq2001 <samsonq2001@gmail.com>
2020-05-07 13:33:09 -0700

    messing around with sprite animation

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -14,13 +14,22 @@ var game = new Phaser.Game(config);
 
 function preload()
 {
-    this.load.image('road', 'images/Road-Background.png');
+    this.load.spritesheet('player', "images/sampleCharacterSpritesCut.png", {frameWidth:32, frameHeight:49})
 }
 
 function create ()
 {
+    this.anims.create({
+        key: 'guy',
+        frames: TouchList.anims.generateFrameNumbers('player', {start: 0, end:1 }),
+        frameRate:2,
+        repeat: -1
+    }); 
     game.scale.resize(window.innerWidth, window.innerHeight);
-    this.add.image((window.innerWidth / 2), (window.innerHeight / 2), 'road');
+    player = this.physics.add.sprite(600, 800, 'player');
+    
+
+    player.setOrigin(0.5, 0.5).setDisplaySize(132, 120).setCollideWorldBounds(true).setDrag(500, 500);
 }
 
 function update ()
