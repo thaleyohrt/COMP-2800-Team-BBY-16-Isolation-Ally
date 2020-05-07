3656657ca599d6faf6dfccf165973b2c21a76a6b
John Vaganov <ivaganov@my.bcit.ca>
2020-05-07 14:20:29 -0700

    Added player movement. Player teleports from lane to lane.

--- scripts/player.js
+++ scripts/player.js
@@ -1,3 +1,6 @@
+var positions = [0, 1, 2];
+var positionCoords = [window.innerWidth / 9, window.innerWidth / 2, window.innerWidth / 1.13];
+
 function loadPlayerAssets(main) {
     main.load.image('player', 'images/john.jpg');
 }
@@ -6,22 +9,22 @@ function addPlayer(main) {
     var player;
     player = main.add.sprite((window.innerWidth / 2), (window.innerHeight / 1.1), 'player');
     
-    player.setOrigin(0.5, 0.5).setDisplaySize(90, 90).setCollideWorldBounds(true).setDrag(500, 500);
+    player.setDisplaySize(90, 90).position = 1;
     
     addMovement(main, player);
 }
 
 function addMovement(main, player) {
-    main.input.keyboard.on('keydown_W', function (event) {
-        player.setAccelerationY(-800);
-    });
-    main.input.keyboard.on('keydown_S', function (event) {
-        player.setAccelerationY(800);
-    });
     main.input.keyboard.on('keydown_A', function (event) {
-        player.setAccelerationX(-800);
+        if (player.position != 0) {
+            player.position = positions[(player.position)-1];
+            player.x = positionCoords[player.position];
+        }
     });
     main.input.keyboard.on('keydown_D', function (event) {
-        player.setAccelerationX(800);
+        if (player.position != 2) {
+            player.position = positions[(player.position)+1];
+            player.x = positionCoords[player.position];
+        }
     });
 }
