94432218dfaf2e004c891fb6881af812cb220f5c
John Vaganov <ivaganov@my.bcit.ca>
2020-05-08 14:02:07 -0700

    Added moving enemies and changed player image.

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -1,26 +1,9 @@
-const FPS = 60;
-const WIDTH = 800;
-const HEIGHT = 600;
-const FONT_SIZE = 40;
-const TEXT_WIDTH = 300;
-const TEXT_Y = 50;
-
-let w = window.innerWidth; // width
-let h = window.innerHeight; // height
-let textX = (w - TEXT_WIDTH) / 2;
-let num = 0;
-let score;
-
 $("body").css("overflow", "hidden");
 let back = [];
-let config = {
+var config = {
     type: Phaser.AUTO,
-    width: WIDTH,
-    height: HEIGHT,
-    fps: {
-        target: FPS,
-        forceSetTimeOut: true
-    },
+    width: 800,
+    height: 600,
     scene: {
         preload: preload,
         create: create,
@@ -28,32 +11,38 @@ let config = {
     }
 };
 
-let game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
-function preload() {
+var w = window.innerWidth ; //width
+var h = window.innerHeight; //height
+var speed = 5; //game speed
+
+function preload()
+{
     this.load.image('road', 'images/Road-Background.png');
     loadPlayerAssets(this);
+    loadEnemyAssets(this);
 }
 
-function create() {
+function create ()
+{
     game.scale.resize(w, h);
     back[0] = this.add.image((w / 2), (h / 2), 'road').setDisplaySize(w, h + 3);
     back[1] = this.add.image((w / 2), -(h / 2), 'road').setDisplaySize(w, h + 3);
     addPlayer(this);
-    score = this.add.text(textX, TEXT_Y, "Score: ", {
-        fontSize: FONT_SIZE + 'px'
-    });
+    
 }
 
-function update() {
-    num++;
-    score.setText("Score: " + (num / 10).toFixed(1) + "ft");
-    back[1].y += 5;
-    back[0].y += 5;
-    if (back[0].y >= h * 1.5) {
+function update ()
+{
+    back[1].y += speed;
+    back[0].y += speed;
+    if (back[0].y >= h * 1.5){
         back[0].y = -(h / 2);
     }
-    if (back[1].y >= h * 1.5) {
+    if (back[1].y >= h * 1.5){
         back[1].y = -(h / 2);
     }
+    spawnEnemies(this);
+    moveEnemies(enemyObjects);
 }
--- scripts/player.js
+++ scripts/player.js
@@ -1,12 +1,12 @@
-let positions = [0, 1, 2];
-let positionCoords = [window.innerWidth / 5, window.innerWidth / 2, window.innerWidth / 1.25];
+var positions = [0, 1, 2];
+var positionCoords = [window.innerWidth / 5, window.innerWidth / 2, window.innerWidth / 1.25];
 
 function loadPlayerAssets(main) {
-    main.load.image('player', 'images/john.jpg');
+    main.load.image('player', 'images/samplePlayer.png');
 }
 
 function addPlayer(main) {
-    let player;
+    var player;
     player = main.add.sprite((window.innerWidth / 2), (window.innerHeight / 1.1), 'player');
     
     player.setDisplaySize(90, 90).position = 1;
@@ -22,18 +22,6 @@ function addMovement(main, player) {
         }
     });
     main.input.keyboard.on('keydown_D', function (event) {
-        if (player.position != 2) {
-            player.position = positions[(player.position)+1];
-            player.x = positionCoords[player.position];
-        }
-    });
-    main.input.keyboard.on('keydown_LEFT', function (event) {
-        if (player.position != 0) {
-            player.position = positions[(player.position)-1];
-            player.x = positionCoords[player.position];
-        }
-    });
-    main.input.keyboard.on('keydown_RIGHT', function (event) {
         if (player.position != 2) {
             player.position = positions[(player.position)+1];
             player.x = positionCoords[player.position];
