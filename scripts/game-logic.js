8dc251b1fba59553b72e93686d872fc1d907b60f
JapaMaron <japamaron@gmail.com>
2020-05-08 10:30:52 -0700

    Added Score system, and added framerate modifiable

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -1,9 +1,15 @@
+let num = 0;
+
 $("body").css("overflow", "hidden");
 let back = [];
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    fps: {
+        target: 60,
+        forceSetTimeOut: true
+    },
     scene: {
         preload: preload,
         create: create,
@@ -13,33 +19,35 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var w = window.innerWidth ; //width
+var w = window.innerWidth; //width
 var h = window.innerHeight; //height
 
-function preload()
-{
+function preload() {
 
     this.load.image('road', 'images/Road-Background.png');
     loadPlayerAssets(this);
 
 }
 
-function create ()
-{
+function create() {
     game.scale.resize(w, h);
     back[0] = this.add.image((w / 2), (h / 2), 'road').setDisplaySize(w, h + 3);
     back[1] = this.add.image((w / 2), -(h / 2), 'road').setDisplaySize(w, h + 3);
     addPlayer(this);
+    score = this.add.text(200, 50, "Score: ", {
+        fontSize: '48px'
+    });
 }
 
-function update ()
-{
+function update() {
+    num++;
+    score.setText("Score: " + num);
     back[1].y += 5;
     back[0].y += 5;
-    if (back[0].y >= h * 1.5){
+    if (back[0].y >= h * 1.5) {
         back[0].y = -(h / 2);
     }
-    if (back[1].y >= h * 1.5){
+    if (back[1].y >= h * 1.5) {
         back[1].y = -(h / 2);
     }
 }
