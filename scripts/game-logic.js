542839354815c7163372bad2ee33cf6391c900d5
Tomas Haley-Ohrt <t.haleyohrt@gmail.com>
2020-05-08 11:55:49 -0700

    Revised score logic and display

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -1,13 +1,24 @@
+const FPS = 60;
+const WIDTH = 800;
+const HEIGHT = 600;
+const FONT_SIZE = 40;
+const TEXT_WIDTH = 300;
+const TEXT_Y = 50;
+
+let w = window.innerWidth; // width
+let h = window.innerHeight; // height
+let textX = (w - TEXT_WIDTH) / 2;
 let num = 0;
+let score;
 
 $("body").css("overflow", "hidden");
 let back = [];
-var config = {
+let config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: WIDTH,
+    height: HEIGHT,
     fps: {
-        target: 60,
+        target: FPS,
         forceSetTimeOut: true
     },
     scene: {
@@ -17,16 +28,11 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
-
-var w = window.innerWidth; //width
-var h = window.innerHeight; //height
+let game = new Phaser.Game(config);
 
 function preload() {
-
     this.load.image('road', 'images/Road-Background.png');
     loadPlayerAssets(this);
-
 }
 
 function create() {
@@ -34,14 +40,14 @@ function create() {
     back[0] = this.add.image((w / 2), (h / 2), 'road').setDisplaySize(w, h + 3);
     back[1] = this.add.image((w / 2), -(h / 2), 'road').setDisplaySize(w, h + 3);
     addPlayer(this);
-    score = this.add.text(200, 50, "Score: ", {
-        fontSize: '48px'
+    score = this.add.text(textX, TEXT_Y, "Score: ", {
+        fontSize: FONT_SIZE + 'px'
     });
 }
 
 function update() {
     num++;
-    score.setText("Score: " + num);
+    score.setText("Score: " + (num / 10).toFixed(1) + "ft");
     back[1].y += 5;
     back[0].y += 5;
     if (back[0].y >= h * 1.5) {
