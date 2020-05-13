e4392b7f8037a8ecc7873b6012dbd29fedb64ad7
JapaMaron <japamaron@gmail.com>
2020-05-13 14:31:47 -0700

    added basic player animations

--- scripts/player.js
+++ scripts/player.js
@@ -2,16 +2,27 @@ let positions = [0, 1, 2];
 let positionCoords = [window.innerWidth / 5, window.innerWidth / 2, window.innerWidth / 1.25];
 
 function loadPlayerAssets(main) {
-    main.load.image('player', 'images/samplePlayer.png');
+    main.load.spritesheet('player', 'images/sampleCharacterSpritesCut.png', {
+        frameWidth: 32,
+        framHeight: 49
+    });
 }
 
 function addPlayer(main) {
     let player;
     player = main.add.sprite((window.innerWidth / 2), (window.innerHeight / 1.1), 'player');
-    
+    main.anims.create({ 
+        key: "player_anim",
+        frames: main.anims.generateFrameNumbers('player'),
+        frameRate: 3,
+        repeat: -1
+    });
+
     player.setDisplaySize(90, 90).position = 1;
     
     addMovement(main, player);
+
+    player.anims.play("player_anim");
 }
 
 function addMovement(main, player) {
