0e174af23f24d858788b92099a1e75be06dc1eac
JapaMaron <japamaron@gmail.com>
2020-05-13 11:55:16 -0700

    Added working pause button

--- scripts/game-logic.js
+++ scripts/game-logic.js
@@ -10,6 +10,8 @@ let h = window.innerHeight; // height
 let textX = (w - TEXT_WIDTH) / 2;
 let num = 0;
 let score;
+let pause;
+let paused = false;
 
 $("body").css("overflow", "hidden");
 let back = [];
@@ -43,23 +45,32 @@ function create() {
     back[0] = this.add.image((w / 2), (h / 2), 'road').setDisplaySize(w, h + 3);
     back[1] = this.add.image((w / 2), -(h / 2), 'road').setDisplaySize(w, h + 3);
     addPlayer(this);
+    this.pause = this.add.text(textX, h / 2, "Pause", {
+        fontSize: FONT_SIZE + 'px'
+    });
+    pointer = game.input.activePointer;
+    this.pause.setInteractive().on('pointerdown', function(){
+        paused = !paused;
+    });
     score = this.add.text(textX, TEXT_Y, "Score: ", {
         fontSize: FONT_SIZE + 'px'
     });
 }
 
 function update() {
-    num++;
-    score.setText("Score: " + (num / 10).toFixed(1) + "ft");
-    back[1].y += speed;
-    back[0].y += speed;
+    if (!paused) {
+        num++;
+        score.setText("Score: " + (num / 10).toFixed(1) + "ft");
+        back[1].y += speed;
+        back[0].y += speed;
 
-    if (back[0].y >= h * 1.5) {
-        back[0].y = -(h / 2);
+        if (back[0].y >= h * 1.5) {
+            back[0].y = -(h / 2);
+        }
+        if (back[1].y >= h * 1.5) {
+            back[1].y = -(h / 2);
+        }
+        spawnEnemies(this);
+        moveEnemies(enemyObjects);
     }
-    if (back[1].y >= h * 1.5) {
-        back[1].y = -(h / 2);
-    }
-    spawnEnemies(this);
-    moveEnemies(enemyObjects);
 }
