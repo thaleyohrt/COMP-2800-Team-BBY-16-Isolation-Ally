b6064340212de0b4fc916885b75547aab4b066fc
JapaMaron <japamaron@gmail.com>
2020-05-18 11:54:19 -0700

    increased hitbox size

--- scripts/enemies.js
+++ scripts/enemies.js
@@ -40,9 +40,12 @@ function moveEnemies(allEnemies) {
 function checkCollision(allEnemies) {
     allEnemies.forEach(function (enemy) {
         if ((enemy.y > (window.innerHeight / 1.1) - 90 && enemy.y < (window.innerHeight / 1.1) + 45) &&
-            ((enemy.x == window.innerWidth / 5 && player.x == window.innerWidth / 5) ||
-                (enemy.x == window.innerWidth / 2 && player.x == window.innerWidth / 2) ||
-                (enemy.x == window.innerWidth / 1.25 && player.x == window.innerWidth / 1.25))) {
+            (((enemy.x >= window.innerWidth / 5 - 45 && player.x >= window.innerWidth / 5 - 45) &&
+                (enemy.x <= window.innerWidth / 5 + 45 && player.x <= window.innerWidth / 5 + 45)) ||
+                ((enemy.x >= window.innerWidth / 2 - 45 && player.x >= window.innerWidth / 2 - 45) &&
+                    (enemy.x <= window.innerWidth / 2 + 45 && player.x <= window.innerWidth / 2 + 45)) ||
+                ((enemy.x >= window.innerWidth / 1.25 - 45 && player.x >= window.innerWidth / 1.25 - 45) &&
+                    (enemy.x <= window.innerWidth / 1.25 + 45 && player.x <= window.innerWidth / 1.25 + 45)))) {
             bgMusic.pause();
             collisionSFX.play();
             highScore();
